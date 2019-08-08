d44f15bac02d153ec4d4cb7899b385872842cfc6
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-08-08 09:42:21 +0900

    sec017 WIP

---
+++ elm-book/ch03/src/sec017.elm
@@ -0,0 +1,28 @@
+module Main exposing (main)
+
+import Html exposing (Html, a, div, h1, li, text, ul)
+import Html.Attributes exposing (href)
+
+
+main : Html msg
+main =
+    div [] [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ linkItem "https://elm-lang.org" "Homepage"
+        , linkItem "https://package.elm-lang.org" "packages"
+        , linkItem "https://ellie-app.com" "Playground"
+        ]
+
+
+linkItem : String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url ] [ text text_ ] ]
