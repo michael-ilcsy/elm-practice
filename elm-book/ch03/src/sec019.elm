274bab347c62ffa3083ed5cc80680006e222c5d6
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-08-08 23:44:29 +0900

    sec019

---
+++ elm-book/ch03/src/sec019.elm
@@ -0,0 +1,74 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (Html, button, div, input, li, text, ul)
+import Html.Attributes exposing (disabled, value)
+import Html.Events exposing (onInput, onSubmit)
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { input : String
+    , memos : List String
+    }
+
+
+init : Model
+init =
+    { input = ""
+    , memos = []
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Input String
+    | Submit
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input input ->
+            { model | input = input }
+
+        Submit ->
+            { model
+                | input = ""
+                , memos = model.input :: model.memos
+            }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.form [ onSubmit Submit ]
+            [ input [ value model.input, onInput Input ] []
+            , button [ disabled (String.isEmpty (String.trim model.input)) ] [ text "Submit" ]
+            ]
+        , ul [] (List.map viewMemo model.memos)
+        ]
+
+
+viewMemo : String -> Html Msg
+viewMemo memo =
+    li [] [ text memo ]
